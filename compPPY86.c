50af442365c37a357e6f4fc1288f8de89c41fdea
Norips <raphdruon@gmail.com>
2017-03-30 13:16:20 +0200

    (feat) Add con, id, arithmetic operator to compPPY86

--- compPPY86.c
+++ compPPY86.c
@@ -8,14 +8,19 @@
 #include "utils/enum.h"
 #include "utils/environ.h"
 
+
+#define STACK_SIZE 256
 static int currentC=0, current=0, currentT=0 ;
 
-void print(int etq,const char *op, const char *arg, const char *arg2, const char *des) {
-    printf("ET%d\t:%s\t:%s\t:%s\t:%s\n",etq,op,arg ? arg : "",arg2 ? arg2 : "",des ? des : "");
+void print(int etq,const char *op, const char *arg, char *arg2) {
+    char buff[128];
+    buff[0] = '\0';
+    strcat(buff,",");
+    printf("ET%d:\t%s %s\t%s\t\n",etq,op,arg ? arg : "",arg2 ? strcat(buff,arg2) : "");
 }
 static int lbJMP=0;
 
-int ex_bis(argType *glob,symbolTag* table,nodeType* node) {
+int ex_bis(symbolTag *glob,symbolTag *loc,nodeType* node) {
     int lbJMP1,lbJMP2,leftCurrent=0, rightCurrent =0;
     char buf[20];
     char buf2[20];
@@ -30,16 +35,14 @@ int ex_bis(argType *glob,symbolTag* table,nodeType* node) {
     }
     else if (node->type == typeCon)
     {
-        
+        char buf[20];
         snprintf(buf,20,"%d",node->con.value);
-        snprintf(buf2,20,"CT%d",++currentC);
-        print(current++,"Afc", buf, NULL, buf2);
+        print(current++,"irmovl",buf,"%eax");
+        
     }
     else if (node->type == typeId)
     {
-        /* Identifier */
-        snprintf(buf,20,"CT%d",++currentC);
-        print(current++,"Af", buf, node->id.id, NULL);
+        print(current++,"mrmovl",node->id.id,"%eax");
     }
     else if (node->type == typeOpr)
     {
@@ -51,68 +54,70 @@ int ex_bis(argType *glob,symbolTag* table,nodeType* node) {
         switch (node->opr.oper)
         {
             case Af:
-                ex_bis(glob,table,opR);
-                break;
-                
-            case Pl:
-                ex_bis(glob,table,opL);
-                ex_bis(glob,table,opR);
+                ex_bis(glob,loc,opR);
+                print(current++,"rmmovl","%eax",opL->id.id);
                 break;
                 
             case Mo:
-                ex_bis(glob,table,opL);
-                ex_bis(glob,table,opR);
-                break;
-            
+            case Pl:
             case Mu:
-                ex_bis(glob,table,opL);
-                ex_bis(glob,table,opR);
-                break;
-
             case Or:
-                ex_bis(glob,table,opL);
-                ex_bis(glob,table,opR);
-                break;
-                
             case And:
-                ex_bis(glob,table,opL);
-                ex_bis(glob,table,opR);
-                break;
+                ex_bis(glob,loc,opL);
+                print(current++,"pushl","%eax",NULL);
+                ex_bis(glob,loc,opR);
+                print(current++,"popl","%ebx",NULL);
+                switch(node->opr.oper) {
+                    case Pl:
+                        print(current++,"addl","%ebx","%eax");
+                        break;
+                    case Mo:
+                        print(current++,"subl","%eax","%ebx");
+                        print(current++,"rrmovl","%ebx","%eax");
+                        break;
+                    case Or:
+                        print(current++,"xorl","%ebx","%eax");
+                        break;
+                    case And:
+                        print(current++,"andl","%ebx","%eax");
+                        break;
+                }
                 
+                break;                
             case If:
-                ex_bis(glob,table,opL);
-                ex_bis(glob,table,opR);
-                ex_bis(glob,table,node->opr.op[2]);
+                ex_bis(glob,loc,opL);
+                ex_bis(glob,loc,opR);
+                ex_bis(glob,loc,node->opr.op[2]);
                 break;
                 
             case Wh:
-                ex_bis(glob,table,opL);
-                ex_bis(glob,table,opR);
+                ex_bis(glob,loc,opL);
+                ex_bis(glob,loc,opR);
                 break;
                 
             case Se:
-                ex_bis(glob,table,opL);
-                ex_bis(glob,table,opR);
+                ex_bis(glob,loc,opL);
+                ex_bis(glob,loc,opR);
                 break;
                 
             case Eq:
-                ex_bis(glob,table,opL);
-                ex_bis(glob,table,opR);
+                ex_bis(glob,loc,opL);
+                ex_bis(glob,loc,opR);
                 break;
  
             case NewAr:
-                ex_bis(glob,table,opR);
+                ex_bis(glob,loc,opR);
                 break;
                 
             case Acc:
-                ex_bis(glob,table,opL);
+                ex_bis(glob,loc,opL);
                 
-                ex_bis(glob,table,opR);
+                ex_bis(glob,loc,opR);
                 break;
                 
             case Aft:
-                ex_bis(glob,table,opR);
-                ex_bis(glob,table,opL);
+                ex_bis(glob,loc,opR);
+                ex_bis(glob,loc,opL);
                 break;
                 
             case Pro:
@@ -134,9 +139,23 @@ int ex_bis(argType *glob,symbolTag* table,nodeType* node) {
 
 
 void ex(argType *glob,symbolTag* table,nodeType* p) {
-    print(current++,"Afc", "0", NULL, "TAS");
-    int res = ex_bis(glob,table,p);
-    print(current++,"St",NULL,NULL,NULL);
+    symbolTag *s,*tmp;
+    //Add global var to global symbol table
+    while(glob != NULL) {
+        var(&table,glob->name,glob->type);
+        glob = (argType*) glob->next;
+    }
+    print(current++,"irmovl","STACK","%esp");
+    int res = ex_bis(table,NULL,p);
+
+    printf(".align 4\n");
+    HASH_ITER(hh,table, s, tmp) {
+        if(s->type == typeVar) {
+            printf("%s:\t.long 0\n",s->name);
+        }
+    }
+    printf(".pos 0x%x\n",(4*current+STACK_SIZE));
+    printf("%s:\t.long 0\n","STACK");
     
 }
 
---
+++ test/test3.pp
@@ -0,0 +1,8 @@
+var X1 : integer,
+var X2 : integer,
+var X3 : integer
+
+X2 := 10 ;
+X1 := X2 ;
+X3 := 5;
+X3 := X1 + X2 - X3
